e92084d8d7b8dd959485240f12d36508f3fc434c
Nathaniel-Haines <nathaniel.b.haines@gmail.com>
2025-03-12 12:41:33 -0400

    patch(io): catch missing risk_basis and insert default
    When converting from `data.frame` to `Triangle` format per `Triangle.wide_data_frame_to_triangle` or `Triangle.long_data_frame_to_triangle`, we assume `risk_basis` is present. By default, if missing, we assume `risk_basis="Accident"`. However, the conversion functions did not actually add the assumed field to the `data.frame` before converting, producing unexpected errors if the user left out `risk_basis`. This patch catches the missing `risk_basis` field, making behavior consistent with the documentation.

--- bermuda/io/data_frame_input.py
+++ bermuda/io/data_frame_input.py
@@ -220,6 +220,8 @@ def wide_data_frame_to_triangle(
             )
         ]
     else:
+        if "risk_basis" not in df.columns:
+            df = df.assign(risk_basis=cell_metadata[0].risk_basis)
         cells = []
         for idx, group_df in df.groupby(
             ["period_start", "period_end", "evaluation_date", "risk_basis"]
@@ -376,6 +378,8 @@ def long_data_frame_to_triangle(
                         values={field: np.array(value)},
                     )
     else:
+        if "risk_basis" not in df.columns:
+            df = df.assign(risk_basis=cell_metadata[0].risk_basis)
         for idx, group_df in df.groupby(
             ["period_start", "period_end", "evaluation_date", "field", "risk_basis"]
             + detail_cols
